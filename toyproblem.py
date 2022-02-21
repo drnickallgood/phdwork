b5b230e72f4eac61387ca0c88ebab63875668f5a
Nick Allgood <nick@Nicks-MBP-2.home>
2022-02-20 19:41:37 -0500

    Had wrong formula, got new expanded formula

--- toyproblem.py
+++ toyproblem.py
@@ -447,6 +447,7 @@ w1h1^2 + 2*w1h1 w1h2 + w1h2^2 + 2*w1h1 w2h3 + 2*w1h2*w2h3 + w2h3^2 +
 -- In the paper, we use w11 and h11.. but for this we will use x0 - x7 as unknowns
 
 We do the norm of ||V-WH||^2
+x0^2 = x0 , idempotency
 
 This is what binds the values in V to WH
 
@@ -457,82 +458,79 @@ This is what binds the values in V to WH
 
 
           WH
-[w1h1 + w2h3  w1h2 + w2h4 ]
-[w3h1 + w4h3  w3h2 + w4h4 ]
-
-becomes
-
 [ x0x4 + x1x6, x0x5 + x1x7 ] 
 [ x2x4 + x3x6, x2x5 + x3x7 ]
 
-x0^2 = x0 , idempotency
 
-V - WH = 
+V - WH  = 
 
 [ 1-x0 x4-x1 x6	2-x0 x5-x1 x7 ]
 [ 3-x2 x4-x3 x6	4-x2 x5-x3 x7 ]
 
-( ((1 - (x0x4)) - (x1x6) ) + ((2 - (x0x5)) - (x1x7)) + ((3 - (x2x4)) - (x3x6)) + ((4 - (x2x5)) - (x3x7)) )^2
+||V-WH||^2_2 = 
+
+( (1-(x0*x4)-(x1*x6))^2 + (2-(x0*x5)-(x1*x7))^2 + (3-(x2*x4)-(x3*x6))^2 + (4-(x2*x5)-(x3*x7))^2 )
+
 
 -- Expanded ---
 
-100 - 20 x0x4 + x0x4^2 - 20 x0x5 + 2 x0x4 x0x5 + x0x5^2 - 20 x1x6 + 
- 2 x0x4 x1x6 + 2 x0x5 x1x6 + x1x6^2 - 20 x1x7 + 2 x0x4 x1x7 + 
- 2 x0x5 x1x7 + 2 x1x6 x1x7 + x1x7^2 - 20 x2x4 + 2 x0x4 x2x4 + 
- 2 x0x5 x2x4 + 2 x1x6 x2x4 + 2 x1x7 x2x4 + x2x4^2 - 20 x2x5 + 
- 2 x0x4 x2x5 + 2 x0x5 x2x5 + 2 x1x6 x2x5 + 2 x1x7 x2x5 + 
- 2 x2x4 x2x5 + x2x5^2 - 20 x3x6 + 2 x0x4 x3x6 + 2 x0x5 x3x6 + 
- 2 x1x6 x3x6 + 2 x1x7 x3x6 + 2 x2x4 x3x6 + 2 x2x5 x3x6 + x3x6^2 - 
- 20 x3x7 + 2 x0x4 x3x7 + 2 x0x5 x3x7 + 2 x1x6 x3x7 + 2 x1x7 x3x7 + 
- 2 x2x4 x3x7 + 2 x2x5 x3x7 + 2 x3x6 x3x7 + x3x7^2
+30 - 2 x0 x4 - 6 x2 x4 + x0^2 x4^2 + x2^2 x4^2 - 4 x0 x5 - 8 x2 x5 + 
+ x0^2 x5^2 + x2^2 x5^2 - 2 x1 x6 - 6 x3 x6 + 2 x0 x1 x4 x6 + 
+ 2 x2 x3 x4 x6 + x1^2 x6^2 + x3^2 x6^2 - 4 x1 x7 - 8 x3 x7 + 
+ 2 x0 x1 x5 x7 + 2 x2 x3 x5 x7 + x1^2 x7^2 + x3^2 x7^2
 
+--- Simplified ---
+
+(-1 + x0 x4 + x1 x6)^2 + (-3 + x2 x4 + x3 x6)^2 + (-2 + x0 x5 + x1 x7)^2 + (-4 + x2 x5 + x3 x7)^2
 
 '''
 
 # 8 unknowns
-
-
 Q5 = {}
 
 #linear coefficients
-Q5['x0', 'x0'] = 
-Q5['x1', 'x1'] = 
-Q5['x2', 'x2'] = 
-Q5['x3', 'x3'] = 
-Q5['x4', 'x4'] = 
-Q5['x5', 'x5'] = 
-Q5['x6', 'x6'] = 
-Q5['x7', 'x7'] = 
+Q5['x0', 'x0'] =
+Q5['x1', 'x1'] =
+Q5['x2', 'x2'] =
+Q5['x3', 'x3'] =
+Q5['x4', 'x4'] =
+Q5['x5', 'x5'] =
+Q5['x6', 'x6'] =
+Q5['x7', 'x7'] =
 
 #Quadratic Coefficients
-Q5['x0', 'x1'] = 
-Q5['x0', 'x2'] = 
-Q5['x0', 'x3'] = 
-Q5['x0', 'x4'] = 
-Q5['x0', 'x5'] = 
-Q5['x0', 'x6'] = 
-Q5['x0', 'x7'] = 
-Q5['x1', 'x2'] = 
-Q5['x1', 'x3'] = 
-Q5['x1', 'x4'] = 
-Q5['x1', 'x5'] = 
-Q5['x1', 'x6'] = 
-Q5['x1', 'x7'] = 
-Q5['x2', 'x3'] = 
-Q5['x2', 'x4'] = 
-Q5['x2', 'x5'] = 
-Q5['x2', 'x6'] = 
-Q5['x2', 'x7'] = 
-Q5['x3', 'x4'] = 
-Q5['x3', 'x5'] = 
-Q5['x3', 'x6'] = 
-Q5['x3', 'x7'] = 
-Q5['x4', 'x5'] = 
-Q5['x4', 'x6'] = 
-Q5['x4', 'x7'] = 
-Q5['x5', 'x6'] = 
-Q5['x5', 'x7'] = 
-Q5['x6', 'x7'] = 
+Q5['x0', 'x1'] =
+Q5['x0', 'x2'] =
+Q5['x0', 'x3'] =
+Q5['x0', 'x4'] =
+Q5['x0', 'x5'] =
+Q5['x0', 'x6'] =
+Q5['x0', 'x7'] =
+Q5['x1', 'x2'] =
+Q5['x1', 'x3'] =
+Q5['x1', 'x4'] =
+Q5['x1', 'x5'] =
+Q5['x1', 'x6'] =
+Q5['x1', 'x7'] =
+Q5['x2', 'x3'] =
+Q5['x2', 'x4'] =
+Q5['x2', 'x5'] =
+Q5['x2', 'x6'] =
+Q5['x2', 'x7'] =
+Q5['x3', 'x4'] =
+Q5['x3', 'x5'] =
+Q5['x3', 'x6'] =
+Q5['x3', 'x7'] =
+Q5['x4', 'x5'] =
+Q5['x4', 'x6'] =
+Q5['x4', 'x7'] =
+Q5['x5', 'x6'] =
+Q5['x5', 'x7'] =
+Q5['x6', 'x7'] =
+
+## Need code to do the power of 2 approximation to input data
+
+
 
 
 
