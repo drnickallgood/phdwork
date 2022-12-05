bb40028b3d46534805df225bdaddd6f4bb0225d7
Nick Allgood <allgood1@umbc.edu>
2022-12-05 18:55:00 -0500

    Added motif-ember dataset extractor for dream server

---
+++ motif-ember.py
@@ -0,0 +1,24 @@
+from ember import read_vectorized_features
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
+import pprint
+import ember
+import random
+
+if __name__ == "__main__":
+
+    # Get EMBER vectors for MOTIF dataset
+    ember_X, y = read_vectorized_features("/media/data1/malware/MOTIF/dataset/", subset="train")
+
+    # Normalize EMBER vectors and apply PCA
+    ember_norm_X = ember_X.copy()
+    ember_norm_X = make_pipeline(MinMaxScaler(), PCA(n_components=2)).fit_transform(ember_norm_X)
+
+    num_samples = 20
+    random.seed(a=0)
+
+    for i in range(0,num_samples):
+        rand_sample = random.randint(0,num_samples)
+        print(ember_norm_X[rand_sample])
+
